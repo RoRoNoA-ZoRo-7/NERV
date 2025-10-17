b349df1edaabb4b744b20124ca080287a63e3521
A N I K E T <129594793+RoRoNoA-ZoRo-7@users.noreply.github.com>
2025-10-17 09:07:39 +0530

    Delete unica/mods/blocklist directory

--- unica/mods/blocklist/customize.sh
+++
@@ -1,2 +0,0 @@
-DELETE_FROM_WORK_DIR "system" "system/etc/ldu_blocklist.xml"
-rm "$APKTOOL_DIR/system/framework/services.jar/smali_classes2/com/samsung/android/server/pm/install/PackageBlockListPolicy\$1.smali"
